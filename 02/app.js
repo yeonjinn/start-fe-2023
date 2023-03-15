c7ec1ed75894b865c2ba6dc0d8ab17b65955d542
yeonjinn <rachel546542@gmail.com>
2023-03-15 23:47:43 +0900

    commit

---
+++ 02/app.js
@@ -0,0 +1,52 @@
+var name = 'Fe'
+var age =28;
+var message = 'hello'+name;
+
+var count =5;
+var nick ="my nick if 'fe'";
+
+var isMan =true;
+console.log(nick=='');
+console.log(!!'test');
+
+var log=function(str){
+    console.log(str);
+};
+function log(str){
+    console.log(str);
+}
+
+var image={};
+image.width=1;
+image.height=2;
+
+image['width']=1;
+
+image['name'+i]='1.jpg'
+image={width: 1, height: 2};
+
+image=[];
+
+image[0]='1.jpg';
+image[1]='2.jpg';
+image=['1.jpg',2,{name: '3.jpg'}];
+
+var month=['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+for (var i=0;i<month.length;i++){
+    console.log(month[i]);
+}
+
+var person={
+    nick: 'peach',
+    age: 25,
+    area: 'jeju',
+    like: 'bin_river'
+}
+console.log(person);
+
+var ott={
+    used: 'netflex',
+    watched: 'the glory',
+    star: '⭐⭐⭐⭐⭐',
+}
+console.log(ott);
